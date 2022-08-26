a83e2543ac754da5ba2bc882206a9b882f9147a5
Alex <jabermudez99@misena.edu.co>
2022-08-25 21:49:47 -0500

    Instalación proyecto
    Se instalada el proyecto Biblioteca y se crea el primer modelo, además de la configuración general y las migraciones.

--- proyecto_1/biblioteca/biblioteca/settings.py
+++ proyecto_1/biblioteca/biblioteca/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.libro'
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-CO'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
