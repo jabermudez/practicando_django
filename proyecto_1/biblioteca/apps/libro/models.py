d5d3886345ca5af1ad786fd1302a49f76b6b0e63
Alex <jabermudez99@misena.edu.co>
2022-08-28 16:53:21 -0500

    Función actualizar y eliminar
    Se realizan las vistas basadas en funciones de actualizar y eliminar.

--- proyecto_1/biblioteca/apps/libro/models.py
+++ proyecto_1/biblioteca/apps/libro/models.py
@@ -12,6 +12,7 @@ class Autor(models.Model):
     apellidos = models.CharField(max_length=220, blank=False, null=False)
     nacionalidad = models.CharField(max_length= 100, blank=False, null=False)
     descripcion = models.TextField(blank=False, null=False)
+    estado = models.BooleanField('Estado', default=True)
     fecha_creacion = models.DateField('Fecha de creación', auto_now =True, auto_now_add = False)
 
 
