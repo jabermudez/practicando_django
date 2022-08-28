7afb720eba8c972dcc27423493fd147738ef9c82
Alex <jabermudez99@misena.edu.co>
2022-08-27 21:17:57 -0500

    Modelo Autor
    Se realiza la creación del modelos Autor y se inicia con el crud de crear y listar autor.

---
+++ proyecto_1/biblioteca/apps/libro/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from django import forms 
+from .models import Autor
+
+class AutorForm(forms.ModelForm):
+    class Meta:
+        model =Autor
+        fields = ['nombre', 'apellidos', 'nacionalidad', 'descripcion']
--- proyecto_1/biblioteca/apps/libro/views.py
+++ proyecto_1/biblioteca/apps/libro/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AutorForm
+from .models import Autor
 
-# Create your views here.
+def Home(request):
+    return render(request, 'index.html')
+
+def crearAutor(request):
+    if request.method == 'POST':
+        autor_form = AutorForm(request.POST)
+        if autor_form.is_valid():
+            autor_form.save()
+            return redirect('index')
+    else:
+        autor_form = AutorForm()
+    return render(request, 'libro/crear_autor.html',{'autor_form':autor_form})
+
+def listarAutor(request):
+    autores = Autor.objects.all()
+    return render(request, 'libro/listar_autor.html', {'autores':autores})
