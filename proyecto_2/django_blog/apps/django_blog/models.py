f1f718dd7cde858b0144da16eb98e2994a61be56
Jose Alexander Bermudez <jabermudez99@misena.edu.co>
2022-08-30 20:17:28 -0500

    Creación Modelos
    Se realiza la creación de los modelos Categoría y Autor.

--- proyecto_2/django_blog/apps/django_blog/models.py
+++ proyecto_2/django_blog/apps/django_blog/models.py
@@ -1,3 +1,39 @@
+from argparse import BooleanOptionalAction
+from lzma import _FilterChain
+from msilib.schema import Class
+from tabnanny import verbose
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField('Nombre de la Categoría', max_length= 100, null=False, blank=False)
+    estado = models.BooleanField('Categoría Activada/Categoría no Activada', default=True)
+    fecha_creacion = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+    
+    def __str__(self):
+        return self.nombre
+
+class Autor(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombres = models.CharField('Nombres del Autor', max_length=255, null=False, blank=False)
+    apellidos = models.CharField('Apellidos de autor', max_length=255, null=False, blank=False)
+    facebook = models.URLField('Facebook', null=True, blank=True)
+    twitter = models.URLField('Twitter', null=True, blank=True)
+    instagram = models.URLField('Instagram', null=True, blank=True)
+    web = models.URLField('Web', null=True, blank=True)
+    correo = models.EmailField('Correo Eletrónico', blank=False, null=False)
+    estado = models.BooleanField('Autor Activo/No Activo', default=True)
+    fecha_creacion = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+    
+    def __str__(self):
+        return "{0},{1}".format(self.apellidos, self.nombres)
