4eb7bd5a78badf3607f09b33d0e917750f51ecf8
Alex <jabermudez99@misena.edu.co>
2022-09-01 21:34:22 -0500

    Confirguracion Modelos
    Se realiza configuración modelo Post y se activan las opciones de busqueda, botones exportar e importar.

--- Proyecto_2/django_blog/apps/blog/models.py
+++ Proyecto_2/django_blog/apps/blog/models.py
@@ -1,6 +1,8 @@
 from argparse import BooleanOptionalAction
+from pyexpat import model
 from tabnanny import verbose
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Categoria(models.Model):
     id = models.AutoField(primary_key=True)
@@ -33,3 +35,22 @@ class Autor(models.Model):
     
     def __str__(self):
         return "{0},{1}".format(self.apellidos, self.nombres)
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField('Título', max_length = 90, blank=False, null=False)
+    slug = models.CharField('Slug', max_length=100, blank=False, null=False)
+    descripcion = models.CharField('Descripción', max_length=110, blank=False, null=False)
+    contenido = RichTextField()
+    imagen = models.URLField(max_length=255, blank=False, null=False)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    estado = models.BooleanField('Publicado/No Publicado', default=True)
+    fecha_creacion = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+    
+    def __str__(self):
+        return self.titulo
--- proyecto_2/django_blog/django_blog/settings.py
+++ proyecto_2/django_blog/django_blog/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.blog'
+    'apps.blog',
+    'import_export',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
