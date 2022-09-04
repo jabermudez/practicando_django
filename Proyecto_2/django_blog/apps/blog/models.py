c2ab18b224068af13c22cd60f7a30540d59a58f6
Alex <jabermudez99@misena.edu.co>
2022-09-03 21:27:50 -0500

    Contenido Post
    Se realiza configuración para agregar contenido a los posts

--- Proyecto_2/django_blog/apps/blog/models.py
+++ Proyecto_2/django_blog/apps/blog/models.py
@@ -41,7 +41,7 @@ class Post(models.Model):
     titulo = models.CharField('Título', max_length = 90, blank=False, null=False)
     slug = models.CharField('Slug', max_length=100, blank=False, null=False)
     descripcion = models.CharField('Descripción', max_length=110, blank=False, null=False)
-    contenido = RichTextField()
+    contenido = RichTextField('Contenido')
     imagen = models.URLField(max_length=255, blank=False, null=False)
     autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
