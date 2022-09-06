4970cef20425b74ac76cdc13a5f5b65a8b26830d
Alex <jabermudez99@misena.edu.co>
2022-09-05 22:28:29 -0500

    Importar_Exportar
    Se agrega la opción importar y exportar en la categoría Post.

--- Proyecto_2/django_blog/apps/blog/admin.py
+++ Proyecto_2/django_blog/apps/blog/admin.py
@@ -21,7 +21,15 @@ class AutorAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     list_display = ('nombres', 'apellidos', 'correo','estado','fecha_creacion',)
     resource_class = AutorResource
 
+class PostResource(resources.ModelResource):
+    class Meta:
+        model = Post
+
+class PostAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    search_fields = ['titulo', 'slug', 'descripcion',]
+    list_display = ('titulo', 'slug', 'descripcion','fecha_creacion',)
+    resource_class = PostResource
 
 admin.site.register(Categoria,CategoriaAdmin)
 admin.site.register(Autor,AutorAdmin)
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
