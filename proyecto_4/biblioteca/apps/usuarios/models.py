37de6632cf8b7cd72ac1ce29c611680c43f2ca00
Alex <jabermudez99@misena.edu.co>
2022-10-09 22:35:51 -0500

    Creando modelo usuario

--- proyecto_4/biblioteca/apps/usuarios/models.py
+++ proyecto_4/biblioteca/apps/usuarios/models.py
@@ -1,3 +1,33 @@
+import email
+from email.mime import image
+from email.policy import default
+from enum import unique
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class Usuario(AbstractBaseUser):
+    username = models.CharField('Nombre de usuario', unique = True, max_length=100)
+    email = models.EmailField('Correo Electrónico', max_length=254, unique=True)
+    nombres = models.CharField('Nombres', max_length=200, blank = True, null = True)
+    apellidos = models.CharField('Apellidos', max_length=200, blank = True, null = True)
+    imagen = models.ImageField('Imagen perfil', upload_to='perfil/', height_field=None, width_fiel=None, max_length=200)
+    usuario_activo = models.BooleanField(default = True)
+    usuario_administrador = models.BooleanField(default = False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'nombres', 'apellidos']
+
+    def __str__(self):
+        return f'{self.nombres},{self.apellidos}'
+    
+    def has_perm(self,perm,obj = None):
+        return True
+    
+    def has_module_perms(self,app_label):
+        return True 
+    
+    @property
+    def is_staff(self):
+        return self.usuario_administrador
