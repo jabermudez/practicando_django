933a59b90004d0b11cc5754388d20ad44f4b1f45
Jose Alexander Bermudez <jabermudez99@misena.edu.co>
2022-10-31 18:10:33 -0500

    Configurando Datatable

--- proyecto_4/biblioteca/static/js/usuarios/index.js
+++ proyecto_4/biblioteca/static/js/usuarios/index.js
@@ -6,7 +6,41 @@ function listadoUsuarios(){
         type:"get",
         dataType:"json",
         success:function(response){
-            $('#tabla_usuarios').DataTable();
+            $('#tabla_usuarios tbody').html("");
+            for(let i = 0; i < response.length;i++){
+                let fila = '<tr>';
+                fila += '<td>' + (i +1 ) + '</td>';
+                fila += '<td>' + response[i]["fields"]['username'] + '</td>';
+                fila += '<td>' + response[i]["fields"]['nombres'] + '</td>';
+                fila += '<td>' + response[i]["fields"]['apellidos'] + '</td>';
+                fila += '<td>' + response[i]["fields"]['email'] + '</td>';
+                fila += '</tr>';
+                $('#tabla_usuarios tbody').append(fila);
+            }
+            $('#tabla_usuarios').DataTable({
+                language: {
+                    decimal: "",
+                    emptyTable: "No hay información",
+                    info: "Mostrando START a END de TOTAL Entradas",
+                    infoEmpty: "Mostrando 0 to 0 of 0 Entradas",
+                    infoFiltered: "(Filtrado de MAX total entradas)",
+                    infoPostFix: "",
+                    thousands: ",",
+                    lengthMenu: "Mostrar _MENU_ Entradas",
+                    loadingRecords: "Cargando...",
+                    processing: "Procesando...",
+                    search: "Buscar:",
+                    zeroRecords: "Sin resultados encontrados",
+                    paginate: {
+                      first: "Primero",
+                      last: "Ultimo",
+                      next: "Siguiente",
+                      previous: "Anterior",
+                    },
+                  },
+          
+            });
+                
         },
         error: function(error){
             console.log(error);
